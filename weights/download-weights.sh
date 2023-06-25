c86feb4a3e133dbc71d8b611be7e8893208e3472
Akash James <akash@iwizardsolutions.com>
2023-06-25 08:35:39 -0700

    donwload weights script

---
+++ weights/download-weights.sh
@@ -0,0 +1,4 @@
+pip3 install gdown
+gdown https://drive.google.com/uc?id=1Z-xJW8jK60vfTlmH4BYKRmL52tLPErJY
+gdown https://drive.google.com/uc?id=1XLgPuX3VW-xMI1ujFCnW9K62MlJ4Fskp
+gdown https://drive.google.com/uc?id=14hBGOuUHDLn5Ur5HUmOf2FlSvaEumvg1
